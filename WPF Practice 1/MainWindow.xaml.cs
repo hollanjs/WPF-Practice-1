48f0f0bd478d19d58cd35c8840c96399f2fda57c
Josh Hollandsworth <jshollandsworth@gmail.com>
2017-03-08 15:17:11 -0500

    added actions to the rest of the buttons

--- WPF Practice 1/MainWindow.xaml.cs
+++ WPF Practice 1/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Threading;
 
 namespace WPF_Practice_1
 {
@@ -33,6 +34,61 @@ namespace WPF_Practice_1
             double d = Math.PI / 2;
 
             myLabel.Text = $"Name: {name}\nx: {x}\nd: {d}";
+        }
+
+        private void button2_Click(object sender, RoutedEventArgs e)
+        {
+            int x = 5;
+
+            if (x == 10)
+            {
+                myLabel.Text = "x is definitely 10!";
+            }
+            else
+            {
+                myLabel.Text = "x is definitely NOT 10!";
+            }
+        }
+
+        private void button3_Click(object sender, RoutedEventArgs e)
+        {
+            int someValue = 4;
+            string name = "John Hollingsworth";
+
+            if ((someValue == 3) && (name == "Josh Hollandsworth"))
+            {
+                myLabel.Text = $"x is 3 and the name is {name}";
+            }
+
+            myLabel.Text = "This line runs no matter what";
+        }
+
+        private void button4_Click(object sender, RoutedEventArgs e)
+        {
+            // figure out later how to implement thread sleep in wpf app
+            int count = 0;
+
+            myLabel.Text = "Starting While Loop to count up ^";
+            //Thread.Sleep(1000);
+
+            while (count < 10)
+            {
+                myLabel.Text = $"Count is currently: {count}";
+                count += 1;
+                //Thread.Sleep(1000);
+            }
+
+            myLabel.Text = "Starting For Loop with subtraction -";
+            //Thread.Sleep(1000);
+
+            for (int i = 0; i < 5; i++)
+            {
+                myLabel.Text = $"Count is currently: {count}";
+                count = count - 1;
+                //Thread.Sleep(1000);
+            }
+
+            myLabel.Text = $"And finally, the count settles at: {count}";
         }
     }
 }
